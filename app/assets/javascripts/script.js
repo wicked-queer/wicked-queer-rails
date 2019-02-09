a2950c7f0c3b2695bb16cc956c67604f468511d7
Steve Rivera <steverivera60@gmail.com>
2019-02-09 13:31:17 -0500

    Add layout

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,26 @@
+$( function() {
+  if (document.getElementById('waypoint')) {
+    var waypoint = new Waypoint({
+      element: document.getElementById('waypoint'),
+      offset: 500,
+      handler: function() {
+        $('.js-header').toggleClass('header--dark');
+      }
+    });
+  }
+
+  $('.js-header-menu').click( function() {
+    $('.js-nav').addClass('is-open');
+    $('.js-main').bind('click', function() {
+      $('.js-nav').removeClass('is-open');
+      $('.js-main').unbind('click');
+    });
+  });
+
+  if ( $('#calendar').length ) {
+    $('#calendar').fullCalendar({
+      events: '/events.json',
+      //defaultView: 'listMonth'
+    });
+  }
+});
