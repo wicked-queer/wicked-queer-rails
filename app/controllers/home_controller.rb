fa9a62f2a09c0791948fb89024564f12e9b1e717
Steve Rivera <steverivera60@gmail.com>
2019-02-09 12:47:08 -0500

    Add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @homePage = HomePage.find('3nFzh4wcHYKa28Sui08msi')
+    @events = Event.all.
+                    params({'fields.date[gte]' => DateTime.now}).
+                    limit(11).
+                    order('date').
+                    load
+    @home = true
   end
 end
