802bc6a45fa9c2896161d1ed30969aad9c515814
Steve Rivera <steverivera60@gmail.com>
2019-03-04 14:27:08 -0500

    Use config var for preview api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper ContentfulRails::MarkdownHelper
 
   def check_preview_api
-    unless Rails.env.production?
+    if ENV['USE_PREVIEW_API']
       ContentfulModel.use_preview_api = true
     end
   end
