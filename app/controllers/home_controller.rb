2984eec128c340497f8c0a6af31f764b9cc26937
Steve Rivera <steverivera60@gmail.com>
2020-03-03 14:57:39 -0500

    Events show up till 3 hours after it starts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     check_preview_api
     @homePage = HomePage.find('3nFzh4wcHYKa28Sui08msi')
     @events = Event.all.
-                    params({'fields.date[gte]' => DateTime.now.to_s}).
+                    params({'fields.date[gte]' => (DateTime.now - 3.hours).to_s}).
                     limit(11).
                     order('date').
                     load
