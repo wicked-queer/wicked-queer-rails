9805d6a9ea3d977e45d92f4186362b19a4e22599
Steve Rivera <steverivera60@gmail.com>
2020-03-03 18:50:25 -0500

    Update grid styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
     check_preview_api
     @homePage = HomePage.find('3nFzh4wcHYKa28Sui08msi')
     @events = Event.all.
-                    params({'fields.date[gte]' => (DateTime.now - 3.hours).to_s}).
-                    limit(11).
+                    params({'fields.date[gt]' => (DateTime.now - 3.hours).to_s}).
+                    limit(12).
                     order('date').
                     load
   end
