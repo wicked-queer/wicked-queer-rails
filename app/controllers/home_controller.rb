d2ac525af688556e8f2512a9d69a21f29065967a
Steve Rivera <steverivera60@gmail.com>
2020-03-03 19:02:27 -0500

    Updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @homePage = HomePage.find('3nFzh4wcHYKa28Sui08msi')
     @events = Event.all.
                     params({'fields.date[gt]' => (DateTime.now - 3.hours).to_s}).
-                    limit(12).
+                    limit(48).
                     order('date').
                     load
   end
