946014117e7fad13c57699f803fa56a73304a4cb
Steve Rivera <steverivera@google.com>
2021-03-20 19:08:38 -0400

    Add special event filter

--- app/controllers/concerns/event_filterable.rb
+++ app/controllers/concerns/event_filterable.rb
@@ -2,7 +2,7 @@ module EventFilterable
   extend ActiveSupport::Concern
 
   def filters
-    [TYPE, VENUE]
+    [SPECIAL_EVENT, TYPE, VENUE]
   end
 
   def filter_events(events)
@@ -20,6 +20,20 @@ module EventFilterable
 
   # The filters ///////////////////////////////////////////////////////////////
 
+  JURIED = {
+    name: 'juried',
+    label: 'Juried',
+    filter: proc { |event, juried| filter_by_juried(event, juried) },
+    options: proc { |events| juried_filter_options(events) }
+  }
+
+  SPECIAL_EVENT = {
+    name: 'special_event',
+    label: 'Special Event',
+    filter: proc { |event, specials| filter_by_special_event(event, specials) },
+    options: proc { |events| special_event_filter_options(events) }
+  }
+
   TYPE = {
     name: 'type',
     label: 'Type',
@@ -36,6 +50,14 @@ module EventFilterable
 
   # filter_by methods /////////////////////////////////////////////////////////
 
+  def self.filter_by_juried(event, juried)
+    juried.include? event.in_competition
+  end
+  
+  def self.filter_by_special_event(event, specials)
+    specials.include? event.special_event
+  end
+
   def self.filter_by_type(event, types)
     types.include? event.event_type
   end
@@ -45,6 +67,28 @@ module EventFilterable
   end
 
   # filter options ////////////////////////////////////////////////////////////
+
+  def self.juried_filter_options(events)
+    [true, false].each do |juried|
+      {
+        value: juried,
+        label: (juried ? 'In Competition' : 'Not in competition'),
+        selected: false,
+        disabled: false
+      }
+    end
+  end
+
+  def self.special_event_filter_options(events)
+    events.map(&:special_event).uniq.compact.map do |special_event|
+      {
+        value: special_event,
+        label: special_event.humanize,
+        selected: false,
+        disabled: false
+      }
+    end
+  end
 
   def self.type_filter_options(events)
     events.map(&:event_type).uniq.compact.map do |type|
