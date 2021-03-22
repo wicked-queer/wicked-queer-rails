0668161ea0aa0384db1e2424fad5db1c4649b240
Steve Rivera <steverivera@google.com>
2021-03-22 08:37:05 -0400

    Switch GA

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
+  include EventSortable
+  include EventFilterable
+  
   def index
     check_preview_api
     @homePage = Home.find('3nFzh4wcHYKa28Sui08msi')
-    @events = Event.upcoming
+    @all_events = Event.upcoming
+    @events = filter_events(@all_events)
+    @events = sort_events(@events)
+
+    @sort_options = sort_options
+    @filters = filters
   end
 end
