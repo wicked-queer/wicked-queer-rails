2b6058c7ba1d1d55a00f77478b8cbbff6eee9a3b
Steve Rivera <steverivera@google.com>
2021-03-26 15:39:37 -0400

    Update analytics

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,26 +1,16 @@
-$( function() {
-  if (document.getElementById('waypoint')) {
-    var waypoint = new Waypoint({
-      element: document.getElementById('waypoint'),
-      offset: 500,
-      handler: function() {
-        $('.js-header').toggleClass('has-background');
-      }
-    });
-  }
+// $( function() {
+//   $('.js-header-menu').click( function() {
+//     $('.js-nav').addClass('is-open');
+//     $('.js-main').bind('click', function() {
+//       $('.js-nav').removeClass('is-open');
+//       $('.js-main').unbind('click');
+//     });
+//   });
 
-  $('.js-header-menu').click( function() {
-    $('.js-nav').addClass('is-open');
-    $('.js-main').bind('click', function() {
-      $('.js-nav').removeClass('is-open');
-      $('.js-main').unbind('click');
-    });
-  });
-
-  if ( $('#calendar').length ) {
-    $('#calendar').fullCalendar({
-      events: '/events.json',
-      //defaultView: 'listMonth'
-    });
-  }
-});
+//   if ( $('#calendar').length ) {
+//     $('#calendar').fullCalendar({
+//       events: '/events.json',
+//       //defaultView: 'listMonth'
+//     });
+//   }
+// });
